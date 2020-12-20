ce1f5ddeb676aae4677a936da87229cbf5d8aa06
iimalinin <malinin.ilja@gmail.com>
2020-12-20 21:27:07 +0300

    Deleted revoked token

--- misc.py
+++ misc.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-TOKEN = "860201377:AAEivV_MOeiJ3lTiu74lsnDgirnQHEDSmow"
+TOKEN = ""
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
